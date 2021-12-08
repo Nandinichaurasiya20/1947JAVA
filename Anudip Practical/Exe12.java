dd58dbc0c3a5ab539fb8d70dc77cfd440f5ae94d
Nandini Chaurasiya <92398550+Nandinichaurasiya20@users.noreply.github.com>
2021-12-08 13:40:49 +0530

    Add files via upload

---
+++ Anudip Practical/Exe12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Exe12 {
+public static void main(String args[])
+{
+float u,v,w,x,y;
+Scanner sc=new Scanner(System.in);
+System.out.prfloatln("Enter First Number");
+u=sc.nextfloat();
+System.out.prfloatln("Enter Second Number");
+v=sc.nextfloat();
+System.out.prfloatln("Enter Third Number");
+w=sc.nextfloat();
+System.out.prfloatln("Enter Forth Number");
+x=sc.nextfloat();
+System.out.prfloatln("Enter Fifth Number");
+y=sc.nextfloat();
+System.out.prfloatln("Average of all the numbers" + (u+v+w+x+y)/5);
+}
+}
